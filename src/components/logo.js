b4c4f30448016ad52d333ce85a3def4bd9184fe1
muca <turaliccmustafa@gmail.com>
2021-07-10 11:18:01 +0200

    first commit

---
+++ src/components/logo.js
@@ -0,0 +1,21 @@
+/** @jsx jsx */
+import { jsx, Image } from "theme-ui";
+import { Link } from "components/link";
+
+export default function Logo({ src, ...rest }) {
+  return (
+    <Link
+      path="/"
+      sx={{
+        variant: "links.Logo",
+        display: "flex",
+        cursor: "pointer",
+        mr: 0,
+        height: "10vh",
+      }}
+      {...rest}
+    >
+      <Image src={src} alt="Landing Page Logo"></Image>
+    </Link>
+  );
+}
